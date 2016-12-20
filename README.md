2250044df085cc79fbfce13e64b33bc97ee7b827
Toshiyuki Suzumura <suz.labo@smoche.sakuraweb.com>
2016-12-20 16:54:28 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+my-flux-todomvc
+===============
+
+Handwriting of a https://github.com/facebook/flux/tree/master/examples/flux-todomvc/
---
+++ src/data/TodoStore.js
@@ -0,0 +1,46 @@
+import Immutable from 'immutable';
+import {ReduceStore} from 'flux/utils';
+import TodoActionTypes from './TodoActionTypes';
+import TodoDispatcher from './TodoDispatcher';
+import Todo from './Todo';
+import Counter from './Counter';
+
+class TodoStore extends ReduceStore {
+  constructor() {
+    super(TodoDispatcher);
+  }
+
+  getInitialState() {
+    return Immutable.OrderedMap();
+  }
+
+  reduce(state, action) {
+    switch (action.type) {
+      case TodoActionTypes.ADD_TODO:
+        // Don't add todos with no text.
+        if (!action.text) {
+          return state;
+        }
+        const id = Counter.increment();
+        return state.set(id, new Todo({
+          id,
+          text: action.text,
+          complete: false,
+        }));
+
+      case TodoActionTypes.DELETE_TODO:
+        return state.delete(action.id);
+
+      case TodoActionTypes.TOGGLE_TODO:
+        return state.update(
+          action.id,
+          todo => todo.set('complete', !todo.complete),
+        );
+
+      default:
+        return state;
+    }
+  }
+}
+
+export default new TodoStore();
